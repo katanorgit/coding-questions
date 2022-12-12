9517424a8c6e0a9b8977bfb06c9bb2e05d0a96e1
Rajkumar Katanor <katanor.raj@gmail.com>
2022-12-11 19:25:08 -0600

    Numberof1Bits

--- src/Numberof1bits.java
+++ src/Numberof1bits.java
@@ -1,15 +1,17 @@
 
 public class Numberof1bits {
 	public static void main(String[] args) {
-		int n=5;
-	System.out.println(	hammingWeight(n));
+		int n = 5;
+		System.out.println(hammingWeight(n));
 	}
+
 	private static int hammingWeight(int n) {
-		 int result=0;
-			while(n!=0) {
-				result+=(n%2)&1;
-				n>>>=1;
-			}
-			return result;
+		int count = 0;
+		for (int i = 0; i <= 32; i++) {
+			count += (n % 2 == 0) ? 0 : 1;
+			n >>>= 1;
+		}
+		return count;
+
 	}
 }
